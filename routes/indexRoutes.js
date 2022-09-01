13ae2a7b64aebf5eda085f210b60a5b91f4f19be
rom <filpoyma@gmail.com>
2022-09-01 15:45:15 +0300

    first commit

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const route = express.Router();
+const db = require('../db/models');
 
-route.get("/", (req, res) => {
-  res.send("Server OK");
+
+route.get("/query", async (req, res) => {
+
+    try {
+      const res = await db.query(`
+   SELECT * from tab    
+   `);
+      console.log('res: ', res);
+    } catch (error) {
+      console.log('Insert Err', error.message);
+      res.json({ status: false, resp: [error.message] });
+    }
 });
 
-route.get("/auth", (req, res) => {
-  // логика определения авторизован ли user
-  res.json({isAuth: true})
-});
 
-route.post("/user", async (req, res) => {
-  try {
-    // await User.create({ //создает обьект, без сохранения в БД
-    //   name: req.body.firstName,
-    //   lastName: req.body.lastName,
-    //
-    // });
-    console.log('User Created');
-    res.json({message: 'успешная запись'});
-  } catch (error) {
-    console.log('\x1b[31m', 'Post url error:', error.message);
-    res.status(500).json({err: error.message})
-  }
-
-
-});
 
 module.exports = route;
